d365bed62c5ffbe1acc97baea54a1f1d9f7439d0
notslave-boy <60798200+notslave-boy@users.noreply.github.com>
2020-10-23 18:26:08 +0300

    добавлен абстрактный класс/класс коллекция

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lab_12
 {
@@ -9,4 +10,43 @@ namespace Lab_12
             Console.WriteLine("Hello World!");
         }
     }
+
+    abstract class Animals  //базовый абстрактный класс
+    {
+        public string Name { get; set; }
+        
+    }
+
+    class Mlek : Animals //наследуемый класс
+    {
+        private string _name;
+        public Mlek(string name)
+        {
+            _name = name;
+        }
+
+    }
+    class Parnokop : Animals //наследуемый класс
+    {
+        private string _name;
+        public Parnokop(string name)
+        {
+            _name = name;
+        }
+
+    }
+    class Bird : Animals //наследуемый класс
+    {
+        private string _name;
+        public Bird(string name)
+        {
+            _name = name;
+        }
+    }
+
+    class Zoo //коллекция объектов дочерних экземпляров класса Animals
+    {
+        List<Animals> objlist = new List<Animals>() { new Mlek("Cat"), new Parnokop("Cow"), new Bird("Pinguine") };       
+    }
+
 }
