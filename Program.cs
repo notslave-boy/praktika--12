5f1b1d90325ba426ae22cdc0c4123e6ed5b5dda7
notslave-boy <60798200+notslave-boy@users.noreply.github.com>
2020-10-23 18:49:27 +0300

    Добавлено поверхностное копирование

--- Program.cs
+++ Program.cs
@@ -7,46 +7,69 @@ namespace Lab_12
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Animals cat = new Mlek("cat");
+            Animals cat2 = (Animals)cat.Clone();
+            cat2.Display();
+            Console.ReadKey();
         }
     }
 
-    abstract class Animals  //базовый абстрактный класс
+    public abstract class Animals : ICloneable
     {
         public string Name { get; set; }
-        
+        public abstract void Display();
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
     }
 
     class Mlek : Animals //наследуемый класс
     {
-        private string _name;
+        public string _name { get; set; }
+
         public Mlek(string name)
         {
             _name = name;
         }
 
+        public override void Display()
+        {
+            Console.WriteLine("Имя животного: " + _name);
+        }
+
     }
     class Parnokop : Animals //наследуемый класс
     {
-        private string _name;
+        public string _name { get; set; }
         public Parnokop(string name)
         {
             _name = name;
+        }
+        public override void Display()
+        {
+            Console.WriteLine("Имя животного: " + _name);
         }
 
     }
     class Bird : Animals //наследуемый класс
     {
-        private string _name;
+        public string _name { get; set; }
         public Bird(string name)
         {
             _name = name;
+        }
+        public override void Display()
+        {
+            Console.WriteLine("Имя животного: " + _name);
         }
     }
 
     class Zoo //коллекция объектов дочерних экземпляров класса Animals
     {
-        List<Animals> objlist = new List<Animals>() { new Mlek("Cat"), new Parnokop("Cow"), new Bird("Pinguine") };       
+        List<Animals> objlist = new List<Animals>() { new Mlek("Cat"), new Parnokop("Cow"), new Bird("Pinguine") };
     }
+
+    
 
 }
